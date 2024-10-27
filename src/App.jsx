e23f50327cb11dfc7eb5b4753b97917030d56bc2
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-10-27 09:55:16 +0600

    add fave icon and change title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Favicon from "react-favicon";
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+      <Favicon  url="https://i.ibb.co.com/KGdk8wg/cricket-1.png"></Favicon>
       <h1 className='text-2xl text-red-500 font-bold bg-slate-400'>BPL-DREAM-11</h1>
       
     </>
